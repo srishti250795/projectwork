ee7450c988acff7d1099e6ae1adeb4cf884a2d56
srishti250795 <srishtisrivastava1994@gmail.com>
2022-09-29 21:06:28 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.poject.lockedme;
+
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\hp\\srishti\\Downloads\\Main"; 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+
+	}
+
+}
